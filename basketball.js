c4671c08da9c095e05f6cab0d688fbf553ddf0d8
shibili0 <90345492+shibili0@users.noreply.github.com>
2022-10-01 03:05:56 +0530

    changed a lot
    Added a better css
    Improvised javaScript
    
    added highlighting option for leading team

--- basketball.js
+++ basketball.js
@@ -1,28 +1,127 @@
-let homeScoreBoard=document.getElementById("homeScore")
-let countHome = 0
-function homeplus1(){
-        countHome += 1
-        homeScoreBoard.textContent = countHome
+let homeScore = document.getElementById("homeScore");
+homeScore.textContent = 0;
+let awayScore = document.getElementById("awayScore");
+awayScore.textContent = 0;
+let homeSum = 0;
+let awaySum = 0;
+function plusOneH() {
+  homeSum += 1;
+  homeScore.textContent = homeSum;
+  if (homeSum > awaySum) {
+    let homeBack = document.getElementById("homeDisplay");
+    homeBack.style.background = "#D1FAE5";
+    let awayBack = document.getElementById("awayDisplay");
+    awayBack.style.background = "black";
+  } else if (awaySum > homeSum) {
+    let awayBack = document.getElementById("awayDisplay");
+    awayBack.style.background = "#D1FAE5";
+    let homeBack = document.getElementById("homeDisplay");
+    homeBack.style.background = "black";
+  } else if ((awaySum = homeSum)) {
+    let awayBack = document.getElementById("awayDisplay");
+    awayBack.style.background = "black";
+    let homeBack = document.getElementById("homeDisplay");
+    homeBack.style.background = "black";
+  }
 }
-function homeplus2(){
-        countHome += 2
-        homeScoreBoard.textContent = countHome
+function plusTwoH() {
+  homeSum += 2;
+  homeScore.textContent = homeSum;
+  if (homeSum > awaySum) {
+        let homeBack = document.getElementById("homeDisplay");
+        homeBack.style.background = "#D1FAE5";
+        let awayBack = document.getElementById("awayDisplay");
+        awayBack.style.background = "black";
+      } else if (awaySum > homeSum) {
+        let awayBack = document.getElementById("awayDisplay");
+        awayBack.style.background = "#D1FAE5";
+        let homeBack = document.getElementById("homeDisplay");
+        homeBack.style.background = "black";
+      } else if ((awaySum = homeSum)) {
+        let awayBack = document.getElementById("awayDisplay");
+        awayBack.style.background = "black";
+        let homeBack = document.getElementById("homeDisplay");
+        homeBack.style.background = "black";
+      }
 }
-function homeplus3(){
-        countHome += 3
-        homeScoreBoard.textContent = countHome
+function plusThreeH() {
+  homeSum += 3;
+  homeScore.textContent = homeSum;
+  if (homeSum > awaySum) {
+        let homeBack = document.getElementById("homeDisplay");
+        homeBack.style.background = "#D1FAE5";
+        let awayBack = document.getElementById("awayDisplay");
+        awayBack.style.background = "black";
+      } else if (awaySum > homeSum) {
+        let awayBack = document.getElementById("awayDisplay");
+        awayBack.style.background = "#D1FAE5";
+        let homeBack = document.getElementById("homeDisplay");
+        homeBack.style.background = "black";
+      } else if ((awaySum = homeSum)) {
+        let awayBack = document.getElementById("awayDisplay");
+        awayBack.style.background = "black";
+        let homeBack = document.getElementById("homeDisplay");
+        homeBack.style.background = "black";
+      }
 }
-let countGuest = 0
-let guestScoreBoard=document.getElementById("guestScore")
-function guestplus1(){
-        countGuest += 1
-        guestScoreBoard.textContent = countGuest
+
+function plusOneA() {
+  awaySum += 1;
+  awayScore.textContent = awaySum;
+  if (homeSum > awaySum) {
+    let homeBack = document.getElementById("homeDisplay");
+    homeBack.style.background = "#D1FAE5";
+    let awayBack = document.getElementById("awayDisplay");
+    awayBack.style.background = "black";
+  } else if (awaySum > homeSum) {
+    let awayBack = document.getElementById("awayDisplay");
+    awayBack.style.background = "#D1FAE5";
+    let homeBack = document.getElementById("homeDisplay");
+    homeBack.style.background = "black";
+  } else if ((awaySum = homeSum)) {
+    let awayBack = document.getElementById("awayDisplay");
+    awayBack.style.background = "black";
+    let homeBack = document.getElementById("homeDisplay");
+    homeBack.style.background = "black";
+  }
 }
-function guestplus2(){
-        countGuest += 2
-        guestScoreBoard.textContent = countGuest
+function plusTwoA() {
+  awaySum += 2;
+  awayScore.textContent = awaySum;
+  if (homeSum > awaySum) {
+        let homeBack = document.getElementById("homeDisplay");
+        homeBack.style.background = "#D1FAE5";
+        let awayBack = document.getElementById("awayDisplay");
+        awayBack.style.background = "black";
+      } else if (awaySum > homeSum) {
+        let awayBack = document.getElementById("awayDisplay");
+        awayBack.style.background = "#D1FAE5";
+        let homeBack = document.getElementById("homeDisplay");
+        homeBack.style.background = "black";
+      } else if ((awaySum = homeSum)) {
+        let awayBack = document.getElementById("awayDisplay");
+        awayBack.style.background = "black";
+        let homeBack = document.getElementById("homeDisplay");
+        homeBack.style.background = "black";
+      }
 }
-function guestplus3(){
-        countGuest += 3
-        guestScoreBoard.textContent = countGuest
+function plusThreeA() {
+  awaySum += 3;
+  awayScore.textContent = awaySum;
+  if (homeSum > awaySum) {
+        let homeBack = document.getElementById("homeDisplay");
+        homeBack.style.background = "#D1FAE5";
+        let awayBack = document.getElementById("awayDisplay");
+        awayBack.style.background = "black";
+      } else if (awaySum > homeSum) {
+        let awayBack = document.getElementById("awayDisplay");
+        awayBack.style.background = "#D1FAE5";
+        let homeBack = document.getElementById("homeDisplay");
+        homeBack.style.background = "black";
+      } else if ((awaySum = homeSum)) {
+        let awayBack = document.getElementById("awayDisplay");
+        awayBack.style.background = "black";
+        let homeBack = document.getElementById("homeDisplay");
+        homeBack.style.background = "black";
+      }
 }
